c9dd099f00251d289830d0bcdfc25555a216879c
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2024-08-03 16:52:31 +0530

    chore: enable tamagui optimizing compiler for expo

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,16 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      [
+        '@tamagui/babel-plugin',
+        {
+          components: ['tamagui'],
+          config: './tamagui.config.ts',
+          logTimings: true,
+          disableExtraction: process.env.NODE_ENV === 'development',
+        },
+      ],
+
       '@babel/plugin-proposal-export-namespace-from',
       'react-native-reanimated/plugin', // react-native-reanimated/plugin has to be listed last.
     ],
